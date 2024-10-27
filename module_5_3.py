709a353d43af1144de55bd4cc1f0d3602dbf593b
Nick-stack517 <gsatello@gmail.com>
2024-10-27 17:03:58 +0700

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,92 @@
+"""
+2023/10/31 00:00|Домашняя работа по уроку "Перегрузка операторов"
+
+Цель: применить знания о перегрузке арифметических операторов и операторов сравнения.
+
+Задача "Нужно больше этажей"
+___________________________________________________________________________________________
+Метод iadd в Python позволяет реализовать сложение с присваиванием (оператор +=).
+Он изменяет сам объект, не создавая новые объекты.
+
+Метод radd используется для сложения, когда экземпляр класса находится справа от оператора +,
+а слева от + не является экземпляром класса.
+"""
+
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.number_of_floors = int(number_of_floors)
+        self.name = name
+
+    def go_to(self, new_floor):
+        if int(new_floor) < 1 or int(new_floor) > self.number_of_floors:
+            print("Такого этажа не существует")
+
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        # print(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            # self.number_of_floors += value
+            self.number_of_floors = self.number_of_floors + value
+            return self
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)  # __eq__
+
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10  # __iadd__
+print(h1)
+
+h2 = 10 + h2  # __radd__
+print(h2)
+
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
